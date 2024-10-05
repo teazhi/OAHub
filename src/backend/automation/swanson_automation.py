3eaa7cca861b796b7fe52612c2ebb61705be38ea
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2024-10-04 23:10:31 -0400

    Testing codegen

--- src/backend/automation/swanson_automation.py
+++ src/backend/automation/swanson_automation.py
@@ -1,6 +1,7 @@
 import time
 from playwright.sync_api import sync_playwright
 from ..utils import create_driver, random_delay, random_mouse_movement
+import random
 
 def automate_swanson(item_link, promotion_code, order_amount, run_amount):
     with sync_playwright() as p:
@@ -11,22 +12,48 @@ def automate_swanson(item_link, promotion_code, order_amount, run_amount):
             page.wait_for_load_state('networkidle')
 
             try:
-                random_mouse_movement()
+                random_mouse_movement(page)
                 random_delay()
-                page.wait_for_selector("xpath=/html/body/div[8]/div/div[2]/div/div/div/div/div/button")
+                try:
+                    page.locator('[aria-label="Close dialog"]').click()
+                except:
+                    print("broken")
                 print("found")
-                page.locator("xpath=/html/body/div[8]/div/div[2]/div/div/div/div/div/button").click()
                 random_delay()
-                random_mouse_movement()
-                page.locator("#quantity").click()
+                random_mouse_movement(page)
+                page.get_by_test_id("addToCartForm").get_by_test_id("SelectBox").select_option("more")
                 random_delay()
-                page.select_option("id=quantity", value=order_amount)
+                page.get_by_label("Quantity").fill(order_amount)
                 random_delay()
-                random_mouse_movement()
-                page.locator("xpath=/html/body/div[1]/div/section/section/section/section/section[1]/section/div[4]/section/div/button").click()
+                random_mouse_movement(page)
+                page.get_by_test_id("addToCartButton").click()
+                random_delay()
+                random_mouse_movement(page)
+                page.get_by_role("button", name="Close", exact=True).click()
+                random_delay()
+                random_mouse_movement(page)
+                page.get_by_test_id("menu-cart-anchor").click()
+                random_delay()
+                random_mouse_movement(page)
+                page.get_by_text("Promotion code", exact=True).click()
+                page.get_by_label("Promotion code").fill("TEST15")
+                random_delay()
+                random_mouse_movement(page)
+                page.get_by_label("Promotion code").press("ControlOrMeta+a")
+                random_delay()
+                random_delay()
+                page.get_by_label("Promotion code").fill(promotion_code)
+                random_delay()
+                random_mouse_movement(page)
+                page.get_by_label("Promotion code").press("Enter")
+                random_delay()
+                random_delay()
+                random_delay()
+                random_delay()
+                random_mouse_movement(page)
+                page.get_by_role("link", name="Proceed to Checkout").click()
+                print("made to checkout")
                 time.sleep(180)
-
-                print("Added product to cart and confirmed.")
             except Exception as e:
                 print(f"Error during the subscription process: {e}")
 
