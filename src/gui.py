d2c9c66bc5120ac499107d2522072295d8fd3e9f
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2024-10-23 21:25:05 -0400

    Moving to PC

--- src/gui.py
+++ src/gui.py
@@ -6,11 +6,13 @@ import io
 import contextlib
 import concurrent.futures
 import json
-from tkinter import filedialog
+import threading
+from tkinter import filedialog, messagebox
 from wholesale import search_skus_from_file
 import re
 from backend.utils import read_json, validate_url, display_error
 from backend.automation_main import start_automation
+import shutil
 
 class DualOutput:
     ansi_escape = re.compile(r'\x1B[@-_][0-?]*[ -/]*[@-~]')
@@ -18,12 +20,11 @@ class DualOutput:
     def __init__(self, text_widget):
         self.text_widget = text_widget
         self.console = sys.stdout
-        bold_font = ('Courier', 10, 'bold')
-        self.text_widget.tag_config("info", foreground="cyan", font=bold_font)
-        self.text_widget.tag_config("success", foreground="green", font=bold_font)
-        self.text_widget.tag_config("warning", foreground="yellow", font=bold_font)
-        self.text_widget.tag_config("error", foreground="red", font=bold_font)
-        self.text_widget.tag_config("503", foreground="red", font=bold_font)
+        self.text_widget.tag_config("info", foreground="cyan")
+        self.text_widget.tag_config("success", foreground="green")
+        self.text_widget.tag_config("warning", foreground="yellow")
+        self.text_widget.tag_config("error", foreground="red")
+        self.text_widget.tag_config("503", foreground="red")
         self.text_widget.tag_config("default", foreground="lightgrey")
 
     def write(self, message):
@@ -76,86 +77,57 @@ def run_main_file(output_widget, sku_file_path, wholesale_button):
         print(f"Error: {e}")
     finally:
         sys.stdout = sys.__stdout__
-        wholesale_button.configure(text="Start", state="normal") 
+        wholesale_button.configure(text="Start", state="normal")
 
-def select_skus_file(output_widget, selected_file_var):
-    file_path = filedialog.askopenfilename(title="Select SKU File", filetypes=(("Text Files", "*.txt"),))
+def select_skus_file(output_textbox, selected_file_var):
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
     if file_path:
-        selected_file_var.set(file_path)  # Update the label with the selected file path
+        selected_file_var.set(file_path)
+        output_textbox.configure(state="normal")
+        output_textbox.insert(ctk.END, f"Selected SKU file: {file_path}\n")
+        output_textbox.configure(state="disabled")
+    else:
+        output_textbox.configure(state="normal")
+        output_textbox.insert(ctk.END, "No file selected.\n")
+        output_textbox.configure(state="disabled")
+
+def download_amazon_links_file(output_widget):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    oldskus_dir = os.path.join(base_dir, "oldskus")
+    
+    try:
+        latest_file = max([f for f in os.listdir(oldskus_dir) if f.startswith("amazon_links_from_skus")], key=lambda f: os.path.getmtime(os.path.join(oldskus_dir, f)))
+        latest_file_path = os.path.join(oldskus_dir, latest_file)
+        download_dir = filedialog.askdirectory(title="Select Download Folder")
+        
+        if download_dir:
+            shutil.copy(latest_file_path, os.path.join(download_dir, latest_file))
+            messagebox.showinfo("Download Complete", f"Latest file {latest_file} has been downloaded successfully.")
+    except ValueError:
+        messagebox.showerror("Error", "No amazon_links_from_skus file found.")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {e}")
 
 def start_automation_file(output_widget, selected_file_var, wholesale_button):
     skus_file_path = selected_file_var.get()
     if skus_file_path:
-        wholesale_button.configure(text="WORKING", state="disabled")  # Disable the button when clicked
+        wholesale_button.configure(text="WORKING", state="disabled") 
         Thread(target=run_main_file, args=(output_widget, skus_file_path, wholesale_button)).start()
 
 def load_stores_config():
     json_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'stores.json')
     return read_json(json_path)
 
-def update_selection(selected_store):
-    global stores, promotion_var, promotion_dropdown, order_amt_dropdown, order_amt_var
-
-    if selected_store in stores:
-        promotions = stores[selected_store].get("promotions", [])
-        if promotions:
-            promotion_dropdown.configure(values=promotions)
-            promotion_var.set(promotions[0])
-        else:
-            promotion_dropdown.configure(values=["Select promotion..."])
-            promotion_var.set("Select promotion...")
-
-        max_order_qty = stores[selected_store].get("max_order_qty", 1)
-        order_qty_values = [str(i) for i in range(1, int(max_order_qty) + 1)]
-        if order_qty_values:
-            order_amt_dropdown.configure(values=order_qty_values)
-            order_amt_var.set(order_qty_values[0])
-        else:
-            order_amt_dropdown.configure(values=["Select order amount..."])
-            order_amt_var.set("Select order amount...")
-
-def start_action():
-    global stores, error_label, order_amt_var
-    selected_store = select_store_var.get()
-    item_link = item_link_var.get()
-    promotion_code = promotion_var.get()
-    order_amount = order_amt_var.get()
-    run_amount = run_amt_var.get()
-
-    if selected_store in stores:
-        base_url = stores[selected_store]["base_url"]
-        if not validate_url(base_url, item_link):
-            error_label.configure(text=f"Error: The item link must start with {base_url}")
-            return
-
-    action_button.configure(text="WORKING", state="disabled")
-    error_label.configure(text="")
-
-    def reset_button():
-        action_button.configure(text="START", state="normal")
-
-    def run_automation():
-        future = executor.submit(start_automation, selected_store, item_link, promotion_code, order_amount, run_amount)
-        root.after(100, check_future, future)
-
-    def check_future(future):
-        if future.done():
-            reset_button()
-        else:
-            root.after(100, check_future, future)
-
-    run_automation()
-
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+def start_home_automation(button):
+    button.configure(text="Working", state="disabled")
+    threading.Timer(2, lambda: button.configure(text="Start", state="normal")).start()
 
 def launch_gui():
-    global form_frame, action_button, root, select_store_var, promotion_dropdown, stores, promotion_var, order_amt_dropdown, run_amt_var, error_label, item_link_var, order_amt_var
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("blue")
 
     root = ctk.CTk()
     root.title("OAHub")
-
     window_width = 800
     window_height = 600
     screen_width = root.winfo_screenwidth()
@@ -165,84 +137,83 @@ def launch_gui():
     root.geometry(f"{window_width}x{window_height}+{position_x}+{position_y}")
     root.resizable(False, False)
 
-    top_title_label = ctk.CTkLabel(root, text="OAHub", font=("Roboto", 40, "bold"))
-    top_title_label.pack(pady=(30, 20))
+    title_frame = ctk.CTkFrame(root)
+    title_frame.pack(pady=20)
 
-    tab_view = ctk.CTkTabview(root, width=800, height=500)
-    tab_view.pack(expand=True, fill="both")
+    top_title_label = ctk.CTkLabel(title_frame, text="OAHub", font=("Roboto", 40, "bold"))
+    top_title_label.pack()
+
+    tab_view = ctk.CTkTabview(root, width=750, height=500)
+    tab_view.pack(expand=True)
 
     home_tab = tab_view.add("Home")
-
     home_tab.grid_columnconfigure(0, weight=1)
-    home_tab.grid_rowconfigure(0, weight=1)
 
     form_frame = ctk.CTkFrame(home_tab)
-    form_frame.grid(row=0, column=0, pady=20, padx=20, sticky="n")
-
-    stores = load_stores_config()
+    form_frame.grid(row=1, column=0, pady=20, padx=20, sticky="n")
 
     select_store_label = ctk.CTkLabel(form_frame, text="Select store:", font=("Roboto", 14))
-    select_store_label.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+    select_store_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
 
     select_store_var = ctk.StringVar(value="Select store...")
-    select_store_dropdown = ctk.CTkComboBox(form_frame, variable=select_store_var, values=list(stores.keys()), width=300)
-    select_store_dropdown.grid(row=1, column=1, padx=10, pady=10, sticky="w")
-    
-    select_store_dropdown.configure(command=lambda _: update_selection(select_store_var.get()))
+    select_store_dropdown = ctk.CTkComboBox(form_frame, variable=select_store_var, values=["Swanson", "iHerb"], width=300)
+    select_store_dropdown.grid(row=0, column=1, padx=10, pady=10, sticky="w")
 
     item_link_label = ctk.CTkLabel(form_frame, text="Item Link:", font=("Roboto", 14))
-    item_link_label.grid(row=2, column=0, padx=10, pady=10, sticky="e")
+    item_link_label.grid(row=1, column=0, padx=10, pady=10, sticky="e")
 
     item_link_var = ctk.StringVar()
     item_link_entry = ctk.CTkEntry(form_frame, textvariable=item_link_var, width=300)
-    item_link_entry.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+    item_link_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
     promotion_label = ctk.CTkLabel(form_frame, text="Promotion:", font=("Roboto", 14))
-    promotion_label.grid(row=3, column=0, padx=10, pady=10, sticky="e")
+    promotion_label.grid(row=2, column=0, padx=10, pady=10, sticky="e")
 
     promotion_var = ctk.StringVar(value="Select promotion...")
     promotion_dropdown = ctk.CTkComboBox(form_frame, variable=promotion_var, values=[""], width=300)
-    promotion_dropdown.grid(row=3, column=1, padx=10, pady=10, sticky="w")
+    promotion_dropdown.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
     order_amt_label = ctk.CTkLabel(form_frame, text="Order amount:", font=("Roboto", 14))
-    order_amt_label.grid(row=4, column=0, padx=10, pady=10, sticky="e")
+    order_amt_label.grid(row=3, column=0, padx=10, pady=10, sticky="e")
 
     order_amt_var = ctk.StringVar(value="Select order amount...")
     order_amt_dropdown = ctk.CTkComboBox(form_frame, variable=order_amt_var, values=[""], width=300)
-    order_amt_dropdown.grid(row=4, column=1, padx=10, pady=10, sticky="w")
+    order_amt_dropdown.grid(row=3, column=1, padx=10, pady=10, sticky="w")
 
     run_amt_label = ctk.CTkLabel(form_frame, text="Times to run:", font=("Roboto", 14))
-    run_amt_label.grid(row=5, column=0, padx=10, pady=10, sticky="e")
+    run_amt_label.grid(row=4, column=0, padx=10, pady=10, sticky="e")
 
     run_amt_var = ctk.StringVar(value="Select number of times to run")
     run_amt_dropdown = ctk.CTkComboBox(form_frame, variable=run_amt_var, values=[str(i) for i in range(1, 11)], width=300)
-    run_amt_dropdown.grid(row=5, column=1, padx=10, pady=10, sticky="w")
+    run_amt_dropdown.grid(row=4, column=1, padx=10, pady=10, sticky="w")
 
-    action_button = ctk.CTkButton(form_frame, text="Start", font=("Roboto", 12, "bold"), width=200, height=50, command=start_action)
-    action_button.grid(row=6, column=0, columnspan=2, pady=20, sticky="n")
+    action_button = ctk.CTkButton(home_tab, text="Start", command=lambda: start_home_automation(action_button), width=200, height=50, font=("Roboto", 12, "bold"))
+    action_button.grid(row=2, column=0, pady=20)
 
-    error_label = ctk.CTkLabel(form_frame, text="", text_color="red", font=("Roboto", 10))
-    error_label.grid(row=7, column=0, columnspan=2, pady=(10, 10), sticky="n")  # Positioned below the Start button
+    error_label = ctk.CTkLabel(home_tab, text="", font=("Roboto", 12), text_color="red")
+    error_label.grid(row=3, column=0, pady=5)
 
     wholesale_tab = tab_view.add("Wholesale")
-
     wholesale_tab.grid_columnconfigure(0, weight=1)
-    wholesale_tab.grid_rowconfigure(0, weight=1)
 
-    output_text = ctk.CTkTextbox(wholesale_tab, width=700, height=400, state="disabled")
-    output_text.grid(row=0, column=0, padx=10, pady=10, sticky="n")
+    wholesale_left_frame = ctk.CTkFrame(wholesale_tab)
+    wholesale_left_frame.grid(row=0, column=0, pady=20, padx=10)
+
+    output_textbox = ctk.CTkTextbox(wholesale_left_frame, width=450, height=250, wrap="word", state="disabled")
+    output_textbox.pack(pady=10)
+
+    wholesale_button = ctk.CTkButton(wholesale_left_frame, text="Start", command=lambda: start_automation_file(output_textbox, selected_file_var, wholesale_button), width=200, height=50)
+    wholesale_button.pack(pady=20)
+
+    wholesale_right_frame = ctk.CTkFrame(wholesale_tab)
+    wholesale_right_frame.grid(row=0, column=1, pady=20, padx=10)
 
     selected_file_var = ctk.StringVar()
 
-    select_file_button = ctk.CTkButton(wholesale_tab, text="Select SKU File", font=("Roboto", 12, "bold"),
-                                       command=lambda: select_skus_file(output_text, selected_file_var))
-    select_file_button.grid(row=1, column=0, pady=20, sticky="n")
+    file_button = ctk.CTkButton(wholesale_right_frame, text="Select SKU File", command=lambda: select_skus_file(output_textbox, selected_file_var))
+    file_button.pack(pady=10)
 
-    selected_file_label = ctk.CTkLabel(wholesale_tab, textvariable=selected_file_var, font=("Roboto", 12))
-    selected_file_label.grid(row=2, column=0, pady=10, sticky="n")
-
-    wholesale_button = ctk.CTkButton(wholesale_tab, text="Start", font=("Roboto", 12, "bold"),
-                                     command=lambda: start_automation_file(output_text, selected_file_var, wholesale_button))
-    wholesale_button.grid(row=3, column=0, pady=20, sticky="n")
+    download_button = ctk.CTkButton(wholesale_right_frame, text="Download", command=lambda: download_amazon_links_file(output_textbox))
+    download_button.pack(pady=10)
 
     root.mainloop()
